dbf4923462ac0b559c9ffd5ce47b39c252bd08f3
Koji Hijikuro <koji0903@gmail.com>
2013-06-01 10:53:40 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FX::Application.config.secret_token = '89359eeb841de5784edc9db00ce6f9576fb3046a599efb88b18a78e021393534edbb4f9fd1a7fb140a910163a9b5229294c195f3146eec8ff5491ef4b9fd0ecd'
