2cbf5f4933dc12e89afff7f30ca7511858ca5099
Koji HIJIKURO <koji0903@gmail.com>
2013-06-14 11:40:09 +0900

    update

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -2,8 +2,9 @@ class Exchange < ActiveRecord::Base
   belongs_to :fx_company
   attr_accessible :kind, :path, :fx_company_id
 
-  has_many :historicals
-
   validates :kind, :presence => true
   validates :path, :presence => true
+
+  has_many :historicals
+
 end
